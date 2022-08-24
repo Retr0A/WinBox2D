c7e8afe0cb22c38cdba037fa9f9598ee670603cb
Retr0A <alexxretr0a@gmail.com>
2022-08-24 10:33:36 +0300

    Added Basic Collision Testing With Example

---
+++ Example/src/Example.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinBox2D.Examples
+{
+    public class Example
+    {
+        static Form form;
+
+        static Panel player;
+
+        public static void Main()
+        {
+            form = new Form()
+            {
+                Text = "Example"
+            };
+
+            Panel box1 = new Panel()
+            {
+                Size = new Size(50, 50),
+                BackColor = Color.Green
+            };
+
+            player = new Panel()
+            {
+                Size = new Size(50, 50),
+                Location = new Point(50, 50),
+                BackColor = Color.Blue
+            };
+
+            if (WinBox2D.CheckCollision(box1, player))
+            {
+                player.BackColor = Color.Red;
+            }
+
+            form.Controls.Add(box1);
+            form.Controls.Add(player);
+
+            Application.Run(form);
+        }
+    }
+}
---
+++ WinBox2D/src/WinBox2D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinBox2D
+{
+    public class WinBox2D
+    {
+        public static bool CheckCollision(Panel a, Panel b)
+        {
+            return a.Bounds.IntersectsWith(b.Bounds);
+        }
+    }
+}
